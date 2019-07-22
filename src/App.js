2e97274b6d469e91cac2f9aa8c175b4dfc42b676
tkas9mbek <kasymbek.rustambekovich@gmail.com>
2019-07-22 13:17:39 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count : 0,
+            value: 1
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const target = event.target;
+
+        this.setState({
+            [target.name]: target.value
+        });
+    }
+
+
+    render() {
+        return (
+            <div className="card mid-cos">
+                <div className="row">
+                    <div className="col-md-12 txt-into-cos" >
+                        <a href="https://github.com/tkas9mbek" style={{'color': 'black'}}> Counter by Kasymbek Tashbaev</a>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <h4 className="txt-cos">{this.state.count}</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="center-block-cos">
+                        <div className="form-group">
+                            <label>Enter amount to increment/decrement</label>
+                            <input type="number"
+                                   name="value"
+                                   className="form-control"
+                                   value={this.state.value}
+                                   onChange={this.handleInputChange}
+                                   min="1"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12 btn-group form-group">
+                        <br />
+                        <button className="btn-primary btn " onClick={() => this.setState({ count: this.state.count + parseFloat(this.state.value)})}> Increment </button>
+                        <button className="btn-primary btn " onClick={() => this.setState({ count: this.state.count - parseFloat(this.state.value)})}> Decrement </button>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-md-12" >
+                        <button className="btn-danger btn" style={{'border-radius': '8px', 'width':'43%', 'float': 'right'}} onClick={() => this.setState({ count: 0, value: 1})}> Reset </button>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
 }
 
 export default App;
